8e3a1f0a1dc4e3fecfbd3b298172109ad1337e6b
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-02-26 21:50:10 +0700

    Add Jenkins tests

---
+++ spec/jenkins/sample_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe package("jenkins") do
+  it { should be_installed }
+end
+
+
+describe service("jenkins") do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+describe package("python-pip") do
+  it { should be_installed }
+end
