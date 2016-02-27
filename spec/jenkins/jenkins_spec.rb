fca98451bf0325153e7f3c275032eda29a83db6f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-02-27 09:17:06 +0700

    Install chromedriver

--- spec/jenkins/jenkins_spec.rb
+++ spec/jenkins/jenkins_spec.rb
@@ -25,3 +25,11 @@ end
 describe package("xvfb") do
   it { should be_installed }
 end
+
+describe file("/usr/bin/chromedriver") do
+  it { should be_exist }
+  it { should be_file }
+  it { should be_executable.by "owner" }
+  it { should be_executable.by "others" }
+  it { should be_executable.by "group" }
+end
